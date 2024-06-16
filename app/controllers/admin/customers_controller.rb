00730d4deecb3d961fb8933054e5b4b4af3ecbd2
shinomiyasayaka <ec2-user@ip-172-31-26-76.ec2.internal>
2024-06-16 10:08:05 +0000

    admin ログイン機能　会員一覧　会員詳細　会員編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,32 @@
 class Admin::CustomersController < ApplicationController
+
+  before_action :authenticate_admin!
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), notice: '会員情報が更新されました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :profile_image, :is_active)
+  end
+
 end
